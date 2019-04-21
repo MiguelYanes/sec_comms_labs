6f9dcba0b5c60c97f109e2cf0c5f83076767bf8b
MiguelYanes <33180147+MiguelYanes@users.noreply.github.com>
2019-04-21 23:43:54 +0100

    Update lab_b.sh

--- Classic Ciphers/lab_b.sh
+++ Classic Ciphers/lab_b.sh
@@ -27,7 +27,7 @@ echo "--------------------------------"
 echo "(:E9 6IA6C:6?46 J@F H:== DE2CE E@ C64@8?:D6 E96 492C24E6C D6ED @7 6249 6?4@5:?8]" > rot47.txt
 cat rot47.txt
 echo " - Decode ROT 47:"
-cat rot47.txt | tr ‘\!-~’ ‘P-~\!-O’
+cat rot47.txt | tr [\!-~] [P-~\!-O]
 
 echo "--------------------------------"
 
@@ -35,7 +35,7 @@ echo "--------------------------------"
 alias rot5='tr [0-9] [5-90-4]'
 alias rot13='tr [A-Z] [N-ZA-M]'
 alias deccaesar='tr [a-z][A-Z] [x-za-w][X-ZA-W]'
-alias rot47='tr ‘\!-~’ ‘P-~\!-O’'
+alias rot47='tr [\!-~] [P-~\!-O]'
 
 cat digits.txt | rot5
 cat rot13.txt | rot13
